ea0fdc65ee71a844445ebb8e88c43bf28620a640
Tobias Schwab <tobias.schwab@dynport.de>
2012-04-04 19:25:24 +0200

    move many methods from Supernova::Solr::Core to Supernova::Solr::Server

--- lib/supernova/solr/core.rb
+++ lib/supernova/solr/core.rb
@@ -1,50 +1,11 @@
+require "supernova/solr/server"
 require "typhoeus"
 require "json"
 
-class Supernova::Solr::Core
+class Supernova::Solr::Core < Supernova::Solr::Server
   attr_reader :solr_url, :name
 
-  class << self
-    # class Page
-    #   attr_reader :url, :protocol
-    #
-    #   def initialize(url, protocol)
-    #     @url = url
-    #     @protocol = protocol
-    #   end
-    #
-    #   def self.get(url)
-    #     Net::HTTP.get(URI.parse(url))
-    #   end
-    #
-    #   def self.post(url, body)
-    #     Net::HTTP.post(URI.parse(url))
-    #   end
-    #
-    #   def self.head(url, protocol)
-    #     Net::HTTP.head(URI.parse(url), protocol)
-    #   end
-    #
-    #   functional_delegate :get, :post,  :attributes => :url
-    #   functional_delegate :head,        :attributes => [:url, :protocol]
-    # end
-    #
-    # Page.new("http://www.dynport.de").get                # delegates to Page.get("http://www.dynport.de")
-    # Page.new("http://www.dynport.de").post("the body")   # delegates to Page.post("http://www.dynport.de", "the body")
-    # Page.new("http://www.dynport.de", "HTTP/1.1").head   # delegates to Page.head("http://www.dynport.de", "HTTP/1.1")
-
-    def functional_delegate(*keys)
-      raise "ERROR: last argument must be e.g. :attributes => [:url]" if !keys.last.is_a?(Hash) || !keys.last.has_key?(:attributes)
-      keys[0..-2].each do |method|
-        define_method(method) do |*args|
-          all_args = [keys.last[:attributes]].flatten.map { |att| self.send(att) } + args
-          self.class.send(method, *all_args)
-        end
-      end
-    end
-  end
-
-  def initialize(solr_url = nil, name = nil)
+  def initialize(solr_url, name)
     @solr_url = Supernova::Solr.remove_trailing_slash(solr_url)
     @name = name
   end
@@ -53,7 +14,6 @@ class Supernova::Solr::Core
     "#{solr_url}/#{name}"
   end
 
-  functional_delegate :commit, :select, :index_docs, :commit, :optimize, :delete_by_query, :truncate, :attributes => :url
   functional_delegate :create, :unload, :status, :attributes => [:solr_url, :name]
 
   class << self
@@ -69,54 +29,7 @@ class Supernova::Solr::Core
       admin_action(solr_url, "STATUS", "core=#{core_name}")
     end
 
-    def index_docs(core_url, docs, commit = false)
-      body = docs.map { |doc| %({"add":) + { :doc => doc }.to_json + "}" }.join("\n")
-      post_update(core_url, body, commit)
-    end
-
-    DEFAULT_PARAMS = {
-      :q => "*:*", :wt => "json"
-    }
-
-    def select(core_url, params = {})
-      JSON.parse(select_raw(core_url, params).body)
-    end
-
-    def select_raw(core_url, params = {})
-      get(core_url, "select", :params => DEFAULT_PARAMS.merge(params))
-    end
-
-    def commit(core_url)
-      post_update(core_url, { "commit" => {} }.to_json)
-    end
-
-    def optimize(core_url)
-      post_update(core_url, { "optimize" => {} }.to_json)
-    end
-
-    def delete_by_query(core_url, query, commit = false)
-      post_update(core_url, { "delete" => { "query" => query }}.to_json, commit)
-    end
-
-    def truncate(core_url, commit = false)
-      delete_by_query(core_url, "*:*", commit)
-    end
-
     private
-
-    def post_update(core_url, body, commit = false)
-      url = "#{core_url}/update/json"
-      if commit.is_a?(Numeric)
-        url << "?commitWithin=#{commit}" 
-      elsif commit
-        url << "?commit=true" 
-      end
-      Typhoeus::Request.post(url, :headers => { "Content-Type" => "application/json" }, :body => body)
-    end
-
-    def get(solr_url, relative_path, attributes = {})
-      Typhoeus::Request.get("#{solr_url}/#{relative_path}", attributes)
-    end
 
     def admin_action(solr_url, action, attributes)
       Typhoeus::Request.get("#{solr_url}/admin/cores?action=#{action}&#{attributes}&wt=json")
--- spec/supernova/solr/server_spec.rb
+++ spec/supernova/solr/server_spec.rb
@@ -15,13 +15,109 @@ describe "Supernova::Solr::Server" do
     end
   end
 
-  
-
   describe "#core_names" do
     it "returns the correct array" do
       stub_request(:get, "http://path.to.solr:112/admin/cores?action=STATUS&wt=json")
         .to_return(:status => 200, :body => project_root.join("spec/fixtures/cores_status.json").read, :headers => {})
       Supernova::Solr::Server.core_names(url).should == %w(project_test supernova_test)
+    end
+  end
+
+  describe "index_docs" do
+    it "indexes the correct rows" do
+      rows = [
+        { "id" => 1, "title_s" => "Peter" },
+        { "id" => 2, "title_s" => "Thomas" },
+      ]
+      stub_request(:post, "http://path.to.solr:112/update/json")
+        .with(
+          :body => "{\"add\":{\"doc\":{\"id\":1,\"title_s\":\"Peter\"}}}\n{\"add\":{\"doc\":{\"id\":2,\"title_s\":\"Thomas\"}}}",
+          :headers => {'Content-Type'=>'application/json'}
+        )
+        .to_return(:status => 200, :body => "", :headers => {})
+      Supernova::Solr::Server.index_docs(url, rows)
+    end
+
+    it "indexes the correct rows with commit" do
+      rows = [
+        { "id" => 1, "title_s" => "Peter" },
+        { "id" => 2, "title_s" => "Thomas" },
+      ]
+      stub_request(:post, "http://path.to.solr:112/update/json?commit=true")
+        .with(
+          :body => "{\"add\":{\"doc\":{\"id\":1,\"title_s\":\"Peter\"}}}\n{\"add\":{\"doc\":{\"id\":2,\"title_s\":\"Thomas\"}}}",
+          :headers => {'Content-Type'=>'application/json'}
+        )
+        .to_return(:status => 200, :body => "", :headers => {})
+      Supernova::Solr::Server.index_docs(url, rows, true)
+    end
+
+    it "indexes the correct rows with commitWithin" do
+      rows = [
+        { "id" => 1, "title_s" => "Peter" },
+        { "id" => 2, "title_s" => "Thomas" },
+      ]
+      stub_request(:post, "http://path.to.solr:112/update/json?commitWithin=10000")
+        .with(
+          :body => "{\"add\":{\"doc\":{\"id\":1,\"title_s\":\"Peter\"}}}\n{\"add\":{\"doc\":{\"id\":2,\"title_s\":\"Thomas\"}}}",
+          :headers => {'Content-Type'=>'application/json'}
+        )
+        .to_return(:status => 200, :body => "", :headers => {})
+      Supernova::Solr::Server.index_docs(url, rows, 10_000)
+    end
+  end
+
+  describe "#select" do
+    let(:body) { { "a" => 1 }.to_json }
+    it "selects with default parameters" do
+      stub_request(:get, "http://path.to.solr:112/select?q=*:*&wt=json")
+        .to_return(:status => 200, :body => body, :headers => {})
+      Supernova::Solr::Server.select(url).should == { "a" => 1 }
+    end
+
+    it "delegates for instance methods" do
+      stub_request(:get, "http://path.to.solr:112/select?q=*:*&wt=json")
+        .to_return(:status => 200, :body => body, :headers => {})
+      Supernova::Solr::Server.new("http://path.to.solr:112").select.should == { "a" => 1 }
+    end
+  end
+
+  it "calls the commit statement" do
+    stub_request(:post, "http://path.to.solr:112/update/json")
+      .with(:body => "{\"commit\":{}}", :headers => {'Content-Type'=>'application/json'})
+      .to_return(:status => 200, :body => "", :headers => {})
+    Supernova::Solr::Server.commit(url)
+  end
+
+  it "delegates commit for instance methods" do
+    stub_request(:post, "http://path.to.solr:112/my_name/update/json")
+      .with(:body => "{\"commit\":{}}", :headers => {'Content-Type'=>'application/json'})
+      .to_return(:status => 200, :body => "", :headers => {})
+    core = Supernova::Solr::Server.new("#{url}/my_name")
+    core.url.should == "#{url}/my_name"
+    core.commit
+  end
+
+  it "calls the optimize statement" do
+    stub_request(:post, "http://path.to.solr:112/update/json")
+      .with(:body => "{\"optimize\":{}}", :headers => {'Content-Type'=>'application/json'})
+      .to_return(:status => 200, :body => "", :headers => {})
+    Supernova::Solr::Server.optimize(url)
+  end
+
+  describe "#delete" do
+    it "deletes by query" do
+      stub_request(:post, "http://path.to.solr:112/update/json")
+        .with(:body => "{\"delete\":{\"query\":\"id:1\"}}", :headers => {'Content-Type'=>'application/json'})
+        .to_return(:status => 200, :body => "", :headers => {})
+      Supernova::Solr::Server.delete_by_query(url, "id:1")
+    end
+
+    it "truncates the index" do
+      stub_request(:post, "http://path.to.solr:112/update/json?commit=true")
+        .with(:body => "{\"delete\":{\"query\":\"*:*\"}}", :headers => {'Content-Type'=>'application/json'})
+        .to_return(:status => 200, :body => "", :headers => {})
+      Supernova::Solr::Server.truncate(url, true)
     end
   end
 end
